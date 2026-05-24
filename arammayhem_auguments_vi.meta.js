44be2ac8341eae94973d76fe88485f67a3ec2a26
sirhvd <huyvd147@gmail.com>
2026-05-24 19:41:48 +0700

    Create arammayhem_auguments_vi.meta.js

---
+++ arammayhem_auguments_vi.meta.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         ARAM Mayhem Augments - Vietnamese Translator
+// @namespace    https://github.com/sirhvd/arammayhem_auguments_vi
+// @version      1.2
+// @description  Việt hóa tên và mô tả các Lõi (Augments) trên MetaSrc.
+// @author       HVD
+// @match        https://www.metasrc.com/lol/arena/build/*
+// @match        https://www.metasrc.com/lol/mayhem/build/*
+// @match        https://www.metasrc.com/lol/arena/tier-list/augments
+// @match        https://www.metasrc.com/lol/mayhem/tier-list/augments
+// @match        https://arammayhem.com/augments/
+// @match        https://arammayhem.com/build/*
+// @match        https://arammayhem.com/combo/*
+// @match        https://arammayhem.com/tools/ryze-simulator/
+// @match        https://arammayhem.com/tools/vladimir-simulator/
+// @grant        GM_xmlhttpRequest
+// @grant        GM_addStyle
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=metasrc.com
+// @homepageURL  https://github.com/sirhvd/arammayhem_auguments_vi
+// @downloadURL  https://raw.githubusercontent.com/sirhvd/arammayhem_auguments_vi/main/arammayhem_auguments_vi.js
+// @updateURL    https://raw.githubusercontent.com/sirhvd/arammayhem_auguments_vi/main/arammayhem_auguments_vi.meta.js
+// @run-at       document-start
+// @license      MIT
+// ==/UserScript==
